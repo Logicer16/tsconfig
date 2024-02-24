433726bb30bfc266cf381e55ba42b4eb99d6f6ce
Logicer <logicer@logicer.cc>
2024-02-24 15:28:03 +1100

    add typechecking to javascript

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/**
+ * @file The prettier config.
+ */
+
+import { prettierConfigGenerator } from "@logicer/prettier-config";
+import { options } from "./eslint.config.js";
+
+export default prettierConfigGenerator(options);
---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+/**
+ * @file The eslint config.
+ */
+// Espree is yet to support import-attributes
+// eslint-disable-next-line import/namespace, import/no-deprecated
+import {ConfigGenerator} from "@logicer/eslint-plugin";
+
+export const options = {
+  ecmaVersion: 2024,
+  eslintPlugin: true,
+  javascript: true,
+  jsdoc: true,
+  prettier: true,
+  typescript: true
+};
+
+const generator = new ConfigGenerator(options);
+
+/**
+ * @type {import("eslint").Linter.FlatConfigFileSpec[]}
+ */
+const ignores = [
+  "node_modules/**/*",
+  "dist/**/*",
+  ".type-coverage/**/*",
+
+  "**/.eslint_report.json",
+  "**/.eslintcache"
+];
+
+/**
+ * @type {import("eslint").Linter.FlatConfig[]}
+ */
+const eslintPluginConfigs = [
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.*.json"],
+        sourceType: "module"
+      }
+    },
+    settings: {
+      "import/parsers": {
+        // Temporary until https://github.com/import-js/eslint-plugin-import/pull/2829
+        espree: [".js", ".jsx", ".cjs", ".mjs"]
+      },
+      "import/resolver": {
+        typescript: {
+          project: ["tsconfig.json", "tsconfig.eslint.json"]
+        }
+      }
+    }
+  },
+  {
+    rules: {
+      "@typescript-eslint/naming-convention": "off"
+    }
+  }
+];
+
+/**
+ * @type {import("eslint").Linter.FlatConfig[]}
+ */
+const config = [
+  {ignores},
+  ...(await generator.config),
+  ...eslintPluginConfigs,
+  ...(await generator.endConfig)
+];
+
+export default config;
